8692d75bb593a3656747686b4db81ad8b0281231
Faril Mammadov <memmedovfaril@gmail.com>
2025-10-14 19:54:22 +0400

    feat: supporters section added to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from '@/components/common/banner';
+import SupportersSection from '@/components/common/supporters-section';
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <SupportersSection />
     </div>
   );
 }
