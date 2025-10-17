12e01eaf9f3fc2c5ed6be8b42264a4a732a37b19
Faril Mammadov <memmedovfaril@gmail.com>
2025-10-17 21:29:56 +0400

    feat: sections added to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import BacklinkExcangeSection from '@/components/common/backlink-exchange';
 import Banner from '@/components/common/banner';
 import DifferentSidesSection from '@/components/common/different-sides-section';
 import FaqSection from '@/components/common/faq-section';
+import HowItWorks from '@/components/common/how-it-works';
 import OfferCard from '@/components/common/offer-card';
 import SolutionsSection from '@/components/common/solutions-section';
 import SupportersSection from '@/components/common/supporters-section';
@@ -11,6 +13,8 @@ export default function Home() {
       <Banner />
       <SupportersSection />
       <SolutionsSection />
+      <HowItWorks />
+      <BacklinkExcangeSection />
       <DifferentSidesSection />
       <FaqSection />
       <OfferCard />
