14fc29b44426e1f534f8e59e45d39e8b39bad604
Faril Mammadov <memmedovfaril@gmail.com>
2025-10-30 18:19:10 +0400

    feat: folder groupping

--- src/components/common/features-section.tsx
+++ src/components/common/features-section.tsx
@@ -15,33 +15,117 @@ const FeaturesSection = () => {
           minima odio, a, est blanditiis iste laborum provident.
         </p>
         <div className="flex flex-col gap-4 mt-10">
-          {Array.from({ length: 5 }).map((_, i) => (
-            <div
-              key={i}
-              className="border border-custom-border rounded-2xl grid md:grid-cols-2 gap-3 p-9 relative overflow-hidden"
-            >
-              <div className="flex flex-col gap-2 relative z-10">
-                <h3 className="text-4xl font-semibold">
-                  Automate SEO analysis and keyword research
-                </h3>
-                <p className="text-sm font-normal text-gray-500">
-                  Analyze and find the best keywords in your niche. Create
-                  quality articles daily matching your business goals. Generate
-                  keywords yourself anytime.
-                </p>
-                <Button className="bg-main hover:bg-main-hover w-fit font-semibold text-md mt-3">
-                  Start for Free
-                </Button>
-              </div>
-              <Image
-                className="absolute md:block hidden top-0 right-0 -rotate-12"
-                src={Works2}
-                alt="Works"
-                width={476}
-                height={476}
-              />
+          <div className="border border-custom-border rounded-2xl grid md:grid-cols-2 gap-3 lg:p-9 md:p-7 p-5 relative overflow-hidden">
+            <div className="flex flex-col gap-2 relative z-10">
+              <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold">
+                Automate SEO analysis and keyword research
+              </h3>
+              <p className="text-sm font-normal text-gray-500">
+                Analyze and find the best keywords in your niche. Create quality
+                articles daily matching your business goals. Generate keywords
+                yourself anytime.
+              </p>
+              <Button className="bg-main hover:bg-main-hover w-fit font-semibold text-md mt-3">
+                Start for Free
+              </Button>
             </div>
-          ))}
+            <Image
+              className="absolute md:block hidden top-0 right-0 -rotate-12"
+              src={Works2}
+              alt="Works"
+              width={476}
+              height={476}
+            />
+          </div>
+          <div className="border border-custom-border rounded-2xl grid md:grid-cols-2 gap-3 lg:p-9 md:p-7 p-5 relative overflow-hidden">
+            <div className="flex flex-col gap-2 relative z-10">
+              <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold">
+                Craft content that ranks organically
+              </h3>
+              <p className="text-sm font-normal text-gray-500">
+                Receive SEO-optimized articles that flow naturally and target
+                strong keywords — each one automatically enriched with strategic
+                internal and external links.
+              </p>
+              <Button className="bg-main hover:bg-main-hover w-fit font-semibold text-md mt-3">
+                Try Examples
+              </Button>
+            </div>
+            <Image
+              className="absolute md:block hidden top-0 right-0 -rotate-12"
+              src={Works2}
+              alt="Works"
+              width={476}
+              height={476}
+            />
+          </div>
+          <div className="border border-custom-border rounded-2xl grid md:grid-cols-2 gap-3 lg:p-9 md:p-7 p-5 relative overflow-hidden">
+            <div className="flex flex-col gap-2 relative z-10">
+              <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold">
+                Create articles that reflect your authentic voice
+              </h3>
+              <p className="text-sm font-normal text-gray-500">
+                Turn your past work into a blueprint — share your articles, and
+                we’ll craft new pieces that speak in your unmistakable voice, as
+                if you wrote them yourself.
+              </p>
+              <Button className="bg-main hover:bg-main-hover w-fit font-semibold text-md mt-3">
+                Start for free
+              </Button>
+            </div>
+            <Image
+              className="absolute md:block hidden top-0 right-0 -rotate-12"
+              src={Works2}
+              alt="Works"
+              width={476}
+              height={476}
+            />
+          </div>
+          <div className="border border-custom-border rounded-2xl grid md:grid-cols-2 gap-3 lg:p-9 md:p-7 p-5 relative overflow-hidden">
+            <div className="flex flex-col gap-2 relative z-10">
+              <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold">
+                Create visuals that perfectly capture your brand’s personality{' '}
+              </h3>
+              <p className="text-sm font-normal text-gray-500">
+                Bring your stories to life with visuals that feel uniquely yours
+                — choose the vibe and colors, and we’ll seamlessly weave them
+                into your articles and highlights.
+              </p>
+              <Button className="bg-main hover:bg-main-hover w-fit font-semibold text-md mt-3">
+                Start for free
+              </Button>
+            </div>
+            <Image
+              className="absolute md:block hidden top-0 right-0 -rotate-12"
+              src={Works2}
+              alt="Works"
+              width={476}
+              height={476}
+            />
+          </div>
+          <div className="border border-custom-border rounded-2xl grid md:grid-cols-2 gap-3 lg:p-9 md:p-7 p-5 relative overflow-hidden">
+            <div className="flex flex-col gap-2 relative z-10">
+              <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold">
+                Set your content on cruise control — publish automatically while
+                you focus on what matters most.{' '}
+              </h3>
+              <p className="text-sm font-normal text-gray-500">
+                Configure it once and let the system handle the rest. Seamlessly
+                connects with WordPress, Webflow, Shopify, Notion, Wix, Framer,
+                and more — your content publishes itself, right on time.
+              </p>
+              <Button className="bg-main hover:bg-main-hover w-fit font-semibold text-md mt-3">
+                Start for free
+              </Button>
+            </div>
+            <Image
+              className="absolute md:block hidden top-0 right-0 -rotate-12"
+              src={Works2}
+              alt="Works"
+              width={476}
+              height={476}
+            />
+          </div>
         </div>
       </div>
     </section>
--- src/components/common/solutions-section.tsx
+++ src/components/common/solutions-section.tsx
@@ -11,6 +11,41 @@ const SolutionsSection = () => {
             Our Solutions for your problems
           </h2>
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-16">
+            <div className="p-9 rounded-2xl bg-white relative border-custom-border border">
+              <h4 className="font-semibold text-3xl text-main">
+                All in one SEO platform
+              </h4>
+              <div className="grid grid-cols-2 gap-5 mt-7">
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Check size={18} />
+                  Keyword Searching{' '}
+                </div>
+              </div>
+            </div>
             <div className="flex flex-col gap-5">
               <div className="p-5 flex items-center gap-5 bg-gradient-to-r from-white via-[#16A34A]/20 to-white border-custom-border border rounded-lg">
                 <Image
@@ -50,41 +85,6 @@ const SolutionsSection = () => {
                   Switching between ChatGPT, Weglot, Surfer, Claude losing time
                   and efficiency.
                 </p>
-              </div>
-            </div>
-            <div className="p-9 rounded-2xl bg-white relative border-custom-border border">
-              <h4 className="font-semibold text-3xl text-main">
-                All in one SEO platform
-              </h4>
-              <div className="grid grid-cols-2 gap-5 mt-7">
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
-                <div className="flex items-center gap-2">
-                  <Check size={18} />
-                  Keyword Searching{' '}
-                </div>
               </div>
             </div>
           </div>
