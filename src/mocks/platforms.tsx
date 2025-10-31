8efb4aef7db18df2e978c19144d88042c1d236c3
Faril Mammadov <memmedovfaril@gmail.com>
2025-10-31 16:13:55 +0400

    feat: platform logos implemented

---
+++ src/mocks/platforms.tsx
@@ -0,0 +1,56 @@
+import Wordpress from '@/assets/images/platform-logos/wordpress.svg';
+import WordpressWeb from '@/assets/images/platform-logos/wordpress-com.svg';
+import Wix from '@/assets/images/platform-logos/wix.svg';
+import Notion from '@/assets/images/platform-logos/notion.svg';
+import Shopify from '@/assets/images/platform-logos/shopify.svg';
+import Framer from '@/assets/images/platform-logos/framer.svg';
+import Sanity from '@/assets/images/platform-logos/sanity.svg';
+import Contentful from '@/assets/images/platform-logos/contenful.svg';
+import Webflow from '@/assets/images/platform-logos/webflow.svg';
+export const platforms = [
+  {
+    id: 1,
+    name: 'Wordpress',
+    image_url: Wordpress,
+  },
+  {
+    id: 2,
+    name: 'Wordpress.com',
+    image_url: WordpressWeb,
+  },
+  {
+    id: 3,
+    name: 'Wix',
+    image_url: Wix,
+  },
+  {
+    id: 4,
+    name: 'Notion',
+    image_url: Notion,
+  },
+  {
+    id: 5,
+    name: 'Shopfiy',
+    image_url: Shopify,
+  },
+  {
+    id: 6,
+    name: 'Framer',
+    image_url: Framer,
+  },
+  {
+    id: 7,
+    name: 'Sanity',
+    image_url: Sanity,
+  },
+  {
+    id: 8,
+    name: 'Webflow',
+    image_url: Webflow,
+  },
+  {
+    id: 9,
+    name: 'Contentful',
+    image_url: Contentful,
+  },
+];
