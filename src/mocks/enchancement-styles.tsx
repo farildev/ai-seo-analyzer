61d49090590fa1b3c7549edd7671aa1ed5fe5ae3
Faril Mammadov <memmedovfaril@gmail.com>
2025-11-03 14:41:33 +0400

    feat: article image type data

---
+++ src/mocks/enchancement-styles.tsx
@@ -0,0 +1,32 @@
+import WaterColorRealism from '@/assets/images/article-images/water-color-realism.png';
+import RealisticStyle from '@/assets/images/article-images/realistic-style.png';
+import CinematicRealism from '@/assets/images/article-images/cinematic-realism.png';
+import IllustrationStyle from '@/assets/images/article-images/illustration.png';
+import SketchStyle from '@/assets/images/article-images/sketch.png';
+export const articleImageStyles = [
+  {
+    id: 1,
+    name: 'Water Color Realism',
+    image_url: WaterColorRealism,
+  },
+  {
+    id: 2,
+    name: 'Cinematic Realism',
+    image_url: CinematicRealism,
+  },
+  {
+    id: 3,
+    name: 'Realistic Style',
+    image_url: RealisticStyle,
+  },
+  {
+    id: 4,
+    name: 'Illustration',
+    image_url: IllustrationStyle,
+  },
+  {
+    id: 5,
+    name: 'Sketch',
+    image_url: SketchStyle,
+  },
+];
