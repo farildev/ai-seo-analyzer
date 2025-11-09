6d2b35514432ed7a76bf6fdd1e747050a907599d
Faril Mammadov <memmedovfaril@gmail.com>
2025-11-10 01:31:50 +0400

    feat: content history page heading added

--- src/app/(dashboard)/dashboard/content-history/page.tsx
+++ src/app/(dashboard)/dashboard/content-history/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-
 const ContentHistory = () => {
-  return <div>ContentHistory</div>;
+  return (
+    <div className="p-5">
+      <div className="flex lg:flex-row flex-col lg:items-center items-start gap-5 justify-between">
+        <div className="flex flex-col gap-3">
+          <h2 className="text-2xl font-semibold">Site Source Configuration</h2>
+          <p className="text-sm font-normal text-gray-400 lg:w-1/2 w-full">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            mollitia nulla corrupti repellat officiis eaque necessitatibus
+            nihil, consequatur vel maiores tenetur doloremque voluptate odit
+            asperiores.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContentHistory;
